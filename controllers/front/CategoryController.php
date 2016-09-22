c85d6835b27c5df6211904e3d1304983e60fd5ec
rachid <rachid@majjane.com>
2016-09-22 16:12:22 +0100

    Commit H

--- controllers/front/CategoryController.php
+++ controllers/front/CategoryController.php
@@ -56,7 +56,10 @@ class CategoryControllerCore extends FrontController
                 _THEME_CSS_DIR_.'product_list.css' => 'all',
             ));
         }
-        //Commentaire Stef  Test Deux push same time V2  22/09/2016
+        //Commentaire V3  22/09/2016
+
+
+        
 
         $scenes = Scene::getScenes($this->category->id, $this->context->language->id, true, false);
         if ($scenes && count($scenes)) {
