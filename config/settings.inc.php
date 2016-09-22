da332579932f569062f22fd1ffc877fdfb5313cc
mustaphamajjane <mustapha@majjane.net.ma>
2016-09-22 09:40:13 +0100

    First Commit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'stefchri_prest50');
+define('_DB_USER_', 'stefchri_prest50');
+define('_DB_PASSWD_', 'j4SpS.p78!');
+define('_DB_PREFIX_', 'psfu_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'jt6dhg0aiyqxc2uixe4nvvxjkkbgzpziaimmkuwtskjhrqnwg19v1je9');
+define('_COOKIE_IV_', '0e1b07ax');
+define('_PS_CREATION_DATE_', '2016-09-22');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.7');
+define('_RIJNDAEL_KEY_', 'SGS6RYXAtCBkj4CT2Kz5mP3Y6dFDzMXc');
+define('_RIJNDAEL_IV_', 'JAHBWfYtdGy4JGtTucaLGw==');
